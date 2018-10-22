77f923636e53f376b59e5ebd324bce5f93889a90
Joshua T <replicaJunction@users.noreply.github.com>
2018-10-22 15:04:05 -0500

    Create run.sh

---
+++ container/run.sh
@@ -0,0 +1,19 @@
+echo "QMK Docker bootstrapper v0.2.4"
+
+(
+    # https://stackoverflow.com/a/1407098
+    export IFS=","
+    for curr_keyboard in $keyboard; do
+        for curr_layout in $layout; do
+            echo
+            echo "================================================================================"
+            echo "Building ${curr_keyboard}:${curr_layout}"
+            echo "================================================================================"
+            echo
+            make ${curr_keyboard}:${curr_layout}
+            cp ${curr_keyboard}_${curr_layout}.hex /output/${curr_keyboard}_${curr_layout}.hex
+        done
+    done
+)
+
+echo "Complete"
